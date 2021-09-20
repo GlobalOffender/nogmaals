68d7a154ad82c80c86e474e40947d190d07eac71
GlobalOffender <99067159@mydavinci.nl>
2021-09-20 14:21:44 +0200

    opdrachten

---
+++ New folder/Opdracht1.py
@@ -0,0 +1,5 @@
+a1 = 30
+while a1 >= 1:
+    print(a1)
+    a1 = a1 - 1
+print("LIFT OFF")
---
+++ New folder/Opdracht5.py
@@ -0,0 +1,5 @@
+question = input("Quit? ").lower
+while question != "quit":
+    question = input("Quit? ")
+    if question == "quit":
+        print("lol")
