cce8d67fa83edaa9e131d822f829800b315f39a8
GlobalOffender <99067159@mydavinci.nl>
2021-09-21 09:42:37 +0200

    coins

---
+++ duizelig/Opdracht2.py
@@ -0,0 +1,11 @@
+print("12 AM")
+am = 1
+while am <= 11:
+    print(str(am) + " AM")
+    am = am + 1
+print("12 PM")
+pm = 1
+while pm <= 11:
+    print(str(pm) + " PM")
+    pm = pm + 1
+
---
+++ duizelig/Opdracht6.py
@@ -0,0 +1,4 @@
+a1 = 50
+while a1 <= 1000:
+        print(a1)
+        a1 = a1 + a1
